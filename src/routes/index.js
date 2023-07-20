2f78c70ae048fad419e557580d007c34656aa2b2
bhaktibuana <bhaktibuana19@gmail.com>
2023-07-20 20:45:20 +0700

    create x-access-token checker & app logger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,18 @@
 const { Router } = require("express");
-const { response } = require("../utils");
+const { response, logFormat, logOptions } = require("../utils");
+const { xAccessTokenCheck } = require("../middlewares");
+const appRoute = require("./app/app.route");
+const morgan = require("morgan");
 
 const router = Router();
+
+router.use(morgan(logFormat, logOptions()));
+router.use("/api", xAccessTokenCheck, appRoute);
+
+router.use("/:anyRoute", (req, res) => {
+  const url = `${req.protocol}://${req.headers.host}${req.originalUrl}`;
+  response(`URL not found for: ${url}`, 404, res);
+});
 
 router.use("/", (req, res) => {
   const url = `${req.protocol}://${req.headers.host}`;
