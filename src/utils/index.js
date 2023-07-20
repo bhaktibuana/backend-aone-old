449f49a780eb47ff2e88b3b772de081d9ba6bf0f
bhaktibuana <bhaktibuana19@gmail.com>
2023-07-20 20:44:06 +0700

    create app logger utility

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,8 @@
 module.exports = {
   response: require("./response.util").response,
   serverErrorResponse: require("./response.util").serverErrorResponse,
+  logFormat: require("./logger.util").logFormat,
+  logOptions: require("./logger.util").logOptions,
+  consoleLog: require("./consoleLog.util"),
+  generateMetadata: require("./generateMetadata.util"),
 };
